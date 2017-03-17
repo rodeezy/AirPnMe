a4e3519e1451bd3d222ce175f88fbb6240717c87
Ryan Odening <ryanodening@Ryans-MacBook-Pro.local>
2017-03-17 12:02:33 -0700

    move description under spot image

--- frontend/components/search/spot_index_item.jsx
+++ frontend/components/search/spot_index_item.jsx
@@ -18,6 +18,7 @@ class IndexItem extends React.Component {
     return (
       <div className="spot-index-item"
            onClick={this.handleClick}>
+        <img src={image_url}/>
         <div className="index-item-info">
           <span className="index-item-category">Rating: </span>
           <span className="index-item-copy">
@@ -26,7 +27,6 @@ class IndexItem extends React.Component {
           <span className="index-item-category">Description: </span>
           <span className="index-item-copy">{description}</span>
         </div>
-        <img src={image_url}/>
       </div>
     );
   }

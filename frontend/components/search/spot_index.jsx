1adff38119dcaa1aafa1dfe3e821da3183c488f6
Ryan Odening <party_on.garth@berkeley.edu>
2017-04-24 21:57:31 -0700

    first commit. cant get pages to re-render

--- frontend/components/search/spot_index.jsx
+++ frontend/components/search/spot_index.jsx
@@ -2,35 +2,49 @@ import React from 'react';
 import SpotIndexItem from './spot_index_item';
 
 const SpotIndex = ({ spots }) => {
-  let i = 0;
+  let page = 0;
 
   const componentDidMount = () => {
-    i = 0;
+    // page = 0;
+    console.log('ayy');
   }
 
   const next = () => {
-    console.log(i);
-    i++;
+    // console.log(i);
+
+    console.log(page);
+    page++;
+    SpotIndex({spots});
   }
+
+  const previous = () => {
+      if (page > 0) {
+        page--;
+      }
+  }
+
   return (
     <div className="spot-results-container">
       <div className='spot-results'>
         <div className='left-half-results'>
-          {spots.slice(0,spots.length/2).map(spot => (
+          {spots.slice(page*9,(page+1)*9).map(spot => (
             <SpotIndexItem spot={spot} key={spot.id} />
           ))}
         </div>
         <div className='right-half-results'>
-          {spots.slice(spots.length/2).map(spot => (
+          {spots.slice((page+1)*9,(page+2)*9).map(spot => (
             <SpotIndexItem spot={spot} key={spot.id} />
           ))}
         </div>
       </div>
       <br />
-      <button>Previous</button>
+      <button onClick={previous}>Previous</button>
       <button onClick={next}>Next</button>
     </div>
   );
 }
 
 export default SpotIndex;
+
+// spots.slice(0,spots.length/2)
+// spots.slice(spots.length/2)

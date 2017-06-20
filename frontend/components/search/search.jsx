8ad6eccd6f845bdd81a13d3be8e3d4260c63d44d
Ryan Odening <party_on.garth@berkeley.edu>
2017-06-19 17:59:32 -0700

    it works! now to add bounds

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -4,22 +4,34 @@ import FilterForm from './filter_form';
 import SpotIndex from './spot_index';
 import SpotMap from './../spot_map/spot_map';
 
-const Search = ({ spots, minPrice, maxPrice, updateFilter, amenities, pageNumber }) => (
-  <div className="user-pane">
-    <div className="left-half">
-      <FilterForm
-        minPrice={minPrice}
-        maxPrice={maxPrice}
-        updateFilter={updateFilter} />
-      <SpotIndex spots={spots} />
+const Search = ({ spots, minPrice, maxPrice, updateFilter, amenities, pageNumber }) => {
+  function handleClick(event) {
+    console.log(`pagenumber ${pageNumber}`);
+    console.log(`value ${event.target.value}`);
+    updateFilter("pageNumber", pageNumber + parseInt(event.target.value))
+  }
+  return (
+    <div className="search-wrapper">
+      <div className="user-pane">
+        <div className="left-half">
+          <FilterForm
+            minPrice={minPrice}
+            maxPrice={maxPrice}
+            updateFilter={updateFilter} />
+          <SpotIndex spots={ spots.slice(pageNumber * 18, (pageNumber + 1) * 18) } />
+        </div>
+        <div className="right-half">
+          <SpotMap
+            spots={ spots.slice(pageNumber * 18, (pageNumber + 1) * 18) }
+            updateFilter={updateFilter}
+            singleSpot={false} />
+        </div>
+      </div>
+      <br />
+      <button onClick={handleClick} value={-1}>Previous</button>
+      <button onClick={handleClick} value={1}>Next</button>
     </div>
-    <div className="right-half">
-      <SpotMap
-        spots={spots}
-        updateFilter={updateFilter}
-        singleSpot={false} />
-    </div>
-  </div>
-);
+  );
+}
 
 export default Search;
--- frontend/components/search/spot_index.jsx
+++ frontend/components/search/spot_index.jsx
@@ -3,15 +3,15 @@ import SpotIndexItem from './spot_index_item';
 // import { receiveSpots } from '../../actions/spot_actions';
 
 class SpotIndex extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      displaySpots: this.props.spots.slice(0,18),
-      page: 0
-    };
-    this.next = this.next.bind(this);
-    this.previous = this.previous.bind(this);
-  }
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     displaySpots: this.props.spots.slice(0,18),
+  //     page: 0
+  //   };
+  //   this.next = this.next.bind(this);
+  //   this.previous = this.previous.bind(this);
+  // }
 
   // componentWillMount(nextProps) {
   //   this.setState({displaySpots: nextProps.spots.slice(0,18)});
@@ -19,37 +19,35 @@ class SpotIndex extends React.Component {
 
   //next and previous need to be redux'd (turned into actions and such)
 
-  next() {
-    // console.log(i);
-    // receiveSpots(spots.slice(18,36));
-    // console.log(page);
-    this.setState({page: this.state.page + 1, displaySpots: this.props.spots.slice(this.state.page*18,
-      (this.state.page+1)*18)});
-  }
-
-  previous() {
-    this.setState({page: this.state.page - 1, displaySpots: this.props.spots.slice(this.state.page*18,
-      (this.state.page+1)*18)});
-  }
+  // next() {
+  //   // console.log(i);
+  //   // receiveSpots(spots.slice(18,36));
+  //   // console.log(page);
+  //   this.setState({page: this.state.page + 1, displaySpots: this.props.spots.slice(this.state.page*18,
+  //     (this.state.page+1)*18)});
+  // }
+  //
+  // previous() {
+  //   this.setState({page: this.state.page - 1, displaySpots: this.props.spots.slice(this.state.page*18,
+  //     (this.state.page+1)*18)});
+  // }
 
   render() {
+    const length = this.props.spots.length
     return (
       <div className="spot-results-container">
         <div className='spot-results'>
           <div className='left-half-results'>
-            {this.state.displaySpots.slice(0, 9).map(spot => (
+            {this.props.spots.slice(0, length/2).map(spot => (
               <SpotIndexItem spot={spot} key={spot.id} />
             ))}
           </div>
           <div className='right-half-results'>
-            {this.state.displaySpots.slice(9,18).map(spot => (
+            {this.props.spots.slice(length/2).map(spot => (
               <SpotIndexItem spot={spot} key={spot.id} />
             ))}
           </div>
         </div>
-        <br />
-        <button onClick={this.previous}>Previous</button>
-        <button onClick={this.next}>Next</button>
       </div>
     );
   }
